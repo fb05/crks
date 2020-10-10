1f422a18ae4abcac863e39377edbc7738e947d5c
fb05 <sgamer3480@gmail.com>
2020-10-10 21:58:49 +0200

    Updates

---
+++ precache-manifest.3c6308b03e310f7cd10cddbbccdfc1a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20e48c4a189ffca8aba2edaa09ec4382",
+    "url": "/crks/index.html"
+  },
+  {
+    "revision": "00f07a61f185a0d2ea64",
+    "url": "/crks/static/css/2.2058ee32.chunk.css"
+  },
+  {
+    "revision": "fe2fc395205227df9497",
+    "url": "/crks/static/css/main.e5714b62.chunk.css"
+  },
+  {
+    "revision": "00f07a61f185a0d2ea64",
+    "url": "/crks/static/js/2.529f6790.chunk.js"
+  },
+  {
+    "revision": "f88b36470b8b650da4a18244e35dd001",
+    "url": "/crks/static/js/2.529f6790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe2fc395205227df9497",
+    "url": "/crks/static/js/main.7feadd09.chunk.js"
+  },
+  {
+    "revision": "dd9cf7578a9bd3083918",
+    "url": "/crks/static/js/runtime-main.c0771496.js"
+  }
+]);
