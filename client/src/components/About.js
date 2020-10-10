70259bf3e8d7be34f608d576d3b25a3da1b18791
fb05 <sgamer3480@gmail.com>
2020-10-10 20:47:37 +0200

    2

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,7 +1,9 @@
 import React from "react";
 // eslint-disable-next-line 
 import { Jumbotron, Container, Row, Col } from "react-bootstrap";
-
+/*<video className="aboutvid" width="960" height="540" controls >
+      <source src="https://www.youtube.com/watch?v=bd7eX2u1WUY&ab_channel=SimonKepp" type="video/mp4"/>
+      </video>*/
 const About = () => (
   <div className="about-container">
   
@@ -11,9 +13,7 @@ const About = () => (
         <h1 className="about-header-title">Markedsfør din hjemmeside</h1>
           
       </Container>
-      <video className="aboutvid" width="960" height="540" controls >
-      <source src="https://www.youtube.com/watch?v=bd7eX2u1WUY&ab_channel=SimonKepp" type="video/mp4"/>
-      </video>
+      
    </div>
   </div>
 );
